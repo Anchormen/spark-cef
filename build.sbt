917d9e05e625a61b8be676c93ffb3234a3838731
Corne Versloot <corne.versloot@gmail.com>
2016-10-07 17:10:15 +0200

    changed scala version to 2.10 to match scala used on the Spark cluster

--- build.sbt
+++ build.sbt
@@ -1,12 +1,10 @@
 name := "spark-cef"
 version := "0.5.23" // <major>.<minor>.<CEF-version>
 organization := "nl.anchormen"
-scalaVersion := "2.10.5"
+scalaVersion := "2.10.6"
 val sparkVersion = "1.6.1"
 
 libraryDependencies ++= Seq(
-	"org.apache.hbase" % "hbase-common" % "1.2.3",
-	"org.apache.hbase" % "hbase-client" % "1.2.3",
 	"org.apache.spark" %% "spark-core" % sparkVersion % "provided",
 	"org.apache.spark" %% "spark-sql" % sparkVersion % "provided",
 	"org.scalatest" %% "scalatest" % "2.2.1" % "test",
--- src/test/scala/nl/anchormen/spark/cef/SimpleTest.scala
+++ src/test/scala/nl/anchormen/spark/cef/SimpleTest.scala
@@ -1,14 +1,10 @@
 package nl.anchormen.spark.cef
 
 import org.apache.spark.SparkConf
-import org.apache.hadoop.hbase.client.Put
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.types.StructType
 import java.text.DateFormat
-import org.apache.hadoop.hbase.HBaseConfiguration
-import org.apache.hadoop.hbase.client.HTable
-import org.apache.hadoop.hbase.util.Bytes
 import org.apache.spark.sql.types.DataType
 import org.apache.spark.sql.types._
 
@@ -16,20 +12,7 @@ object SimpleTest extends App {
   val conf = new SparkConf().setAppName("CEF Test").setMaster("local[*]")
   val sc = new SparkContext(conf)
   val sqlContext = new SQLContext(sc)
-  /*
   
-  val lines = sc.textFile("src/test/resources/corne.log",1).zipWithIndex().map(_.swap)
-  val records = lines.map{case(id, value) => {(id, Array(id, "data", "value", value))}}
-  records.take(2).foreach(println)
- 
-  val hbconf = HBaseConfiguration.create()
-  val myTable = new HTable(hbconf, "cef")
-  var p = new Put(new String("CEF_1").getBytes());
-  p.add(Bytes.toBytes("data"), Bytes.toBytes("column"), Bytes.toBytes("value"));
-  myTable.put(p);
-  myTable.flushCommits();
-  
-   */
   val data = sqlContext.read.format("nl.anchormen.spark.cef.CefSource")
     //.option("scanLines", "1")
     .option("partitions", "2")
@@ -38,6 +21,8 @@ object SimpleTest extends App {
     .load("src/test/resources/corne.log")
   data.select("eventAnnotationEndTime").rdd.take(10).foreach(println)
   data.printSchema()
+  
+  
   /*
   val fields = data.schema.fields.map(struct => (Bytes.toBytes(struct.name), struct.dataType))
   
